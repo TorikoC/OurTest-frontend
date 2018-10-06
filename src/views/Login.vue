71d313bdc4d58ee07a05b35c7fa7b7978c0ec0de
tk <tk@localhost.localdomain>
2018-10-06 14:58:20 +0800

    chore(): 支持登陆注册

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <form action="">
+    <form action="" id='loginForm' name='loginForm'>
       <div>
         <label for="email">Email:</label>
-        <input type="text" id="username" name="username">
+        <input type="text" id="email" name="email">
       </div>
       <div>
         <label for="password">Password</label>
         <input type="text" id="password" name="password">
       </div>
       <div>
-        <button @click.prevent="submit">Login</button>
+        <button type="submit" @click.prevent="submit">Login</button>
       </div>
       <div>
         <router-link to="/Register">Register</router-link>
@@ -20,11 +20,31 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  
-};
-</script>
+  methods: {
+    submit(event) {
+      const form = document.getElementById('loginForm');
+      const formData = new FormData(form);
+      axios.post('http://localhost:3000/login', formData)
+        .then((resp) => {
+          if (resp.data === 'error') {
+            alert('email or password wrong');
+            return;
+          }
+          const { token } = resp.data;
+          localStorage.setItem('jwt', token);
+          this.$router.push('/home');
+        })
+        .catch((err) => {
+          console.log('err', err);
+        });
+      },
+    },
+  };
+  </script>
 
-<style>
+  <style>
 
-</style>
+  </style>
